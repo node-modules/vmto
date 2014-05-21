26d722c9ee978c52fb52501ec3c43e50c2ac1d25
fengmk2 <fengmk2@gmail.com>
2014-05-21 13:21:54 +0800

    support slient mode also

--- test/nunjucks.test.js
+++ test/nunjucks.test.js
@@ -19,6 +19,11 @@ var vmto = require('../');
 
 describe('nunjucks.test.js', function () {
   it('should convert Reference', function () {
+    vmto.nunjucks("hello, $name!").should.equal('hello, {{name}}!');
     vmto.nunjucks("hello, ${name}!").should.equal('hello, {{name}}!');
+    vmto.nunjucks("hello, ${name}! ${action} ops...").should.equal('hello, {{name}}! {{action}} ops...');
+
+    vmto.nunjucks("hello, $!name!").should.equal('hello, {{name}}!');
+    vmto.nunjucks("hello, $!{name}!").should.equal('hello, {{name}}!');
   });
 });
